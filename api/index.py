e7d315dd59ae01dfee2c36ff636bc66f13ddd45e
VasudevS4401 <vasudevs2005@gmail.com>
2025-05-25 17:15:59 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import os
+from typing import List
 
 app = FastAPI()
 
@@ -13,14 +14,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Make sure path is correct relative to this file
 json_path = os.path.join(os.path.dirname(__file__), "marks.json")
-
 with open(json_path, "r") as file:
     data = json.load(file)
 
 @app.get("/api")
-def get_marks(name: list[str] = []):
+def get_marks(name: List[str] = Query([])):
     results = []
     for n in name:
         for entry in data:
@@ -28,5 +27,5 @@ def get_marks(name: list[str] = []):
                 results.append({"name": n, "marks": entry["marks"]})
                 break
         else:
-            results.append({"name": n, "marks": None})  # Not found
+            results.append({"name": n, "marks": None})
     return {"results": results}
